1fb41315331ce51bb8cd951ab3bd5f66e805c1b2
Predrag Drljaca <hello@toodear.rocks>
2026-03-05 22:53:53 +0100

    Checkpoint: stable hidden-titlebar window styling

--- Marky/MarkyApp.swift
+++ Marky/MarkyApp.swift
@@ -7,9 +7,6 @@
 
 import SwiftUI
 import SwiftData
-#if os(macOS)
-import AppKit
-#endif
 
 @main
 struct MarkyApp: App {
@@ -29,38 +26,11 @@ struct MarkyApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                #if os(macOS)
-                .background(WindowChromeConfigurator())
-                #endif
         }
         .modelContainer(sharedModelContainer)
+        #if os(macOS)
+        .windowStyle(.hiddenTitleBar)
+        .windowToolbarStyle(.unified(showsTitle: false))
+        #endif
     }
 }
-#if os(macOS)
-private struct WindowChromeConfigurator: NSViewRepresentable {
-    func makeNSView(context: Context) -> NSView {
-        let view = NSView()
-        DispatchQueue.main.async {
-            configureWindow(from: view)
-        }
-        return view
-    }
-
-    func updateNSView(_ nsView: NSView, context: Context) {
-        DispatchQueue.main.async {
-            configureWindow(from: nsView)
-        }
-    }
-
-    private func configureWindow(from view: NSView) {
-        guard let window = view.window else { return }
-        window.styleMask.insert(.fullSizeContentView)
-        window.titlebarAppearsTransparent = true
-        window.titleVisibility = .hidden
-        window.toolbarStyle = .unified
-        window.toolbar?.showsBaselineSeparator = false
-        window.isOpaque = false
-        window.backgroundColor = NSColor(calibratedWhite: 0.1, alpha: 1.0)
-    }
-}
-#endif
