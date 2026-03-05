c86bb90f327618feeec8953faead399818edb252
Predrag Drljaca <hello@toodear.rocks>
2026-03-05 13:38:26 +0100

    Initial Commit

---
+++ Marky/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Marky
+//
+//  Created by Predrag Drljaca on 3/5/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MarkyTests/MarkyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarkyTests.swift
+//  MarkyTests
+//
+//  Created by Predrag Drljaca on 3/5/26.
+//
+
+import Testing
+@testable import Marky
+
+struct MarkyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
